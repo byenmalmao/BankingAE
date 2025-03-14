383db13e3c28021cbaefcab1a2b304bacfe8b74e
Arg-Torres <20220847@miucateci.edu.do>
2025-03-14 10:55:05 -0400

    Se actualizo?

--- Argenis.py
+++ Argenis.py
@@ -1,6 +1,16 @@
-print("Eres puta?")
-respuesta = input("Responde si o no: ")
-if respuesta == "si":
-    print ("OMG")
-else:
-    print("lAstima")
+from flask import Flask 
+
+app = Flask(__name__)
+@app.route('/')
+
+def puta():
+    print("Eres puta?")
+    respuesta = input("Responde si o no: ")
+    if respuesta == "si":
+        print ("OMG")
+    else:
+     print("lAstima")
+
+@app.route('/hola')
+def hola():
+   print("MMG")
