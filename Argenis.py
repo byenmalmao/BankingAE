917a93ebfa4ede34405a306b425707d2243eed26
Arg-Torres <20220847@miucateci.edu.do>
2025-03-17 11:37:23 -0400

    Probando

--- Argenis.py
+++ Argenis.py
@@ -1,8 +1,12 @@
-from flask import Flask 
-
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
+def index():
+   return render_template('BankingAE/index.html')
+
+
+@app.route('/ay')
 def puta():
     print("Eres puta?")
     respuesta = input("Responde si o no: ")
