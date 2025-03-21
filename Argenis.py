0e5886491927eec21ead351f84c5b5f27ff643f1
trancyenma <148397318+trancyenma@users.noreply.github.com>
2025-03-21 09:18:14 -0400

    Enma/Login

--- Argenis.py
+++ Argenis.py
@@ -2,23 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def index():
-   return render_template('BankingAE/index.html')
+   return render_template('index.html')  # No incluyas 'templates/'
 
+@app.route('/login')
+def Login():
+   return render_template('login.html')
 
-@app.route('/ay')
-def puta():
-    print("Eres puta?")
-    respuesta = input("Responde si o no: ")
-    if respuesta == "si":
-        print ("OMG")
-    else:
-     print("lAstima")
-
-@app.route('/hola')
-def hola():
-   print("MMG")
-
-if __name__ == '__main__':
-    app.run(debug = True)
+app.run(host="0.0.0.0", port=50100, debug=True)
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'scotia-blue': '#003366', // Color personalizado inspirado en Scotia Bank
+            },
+        },
+    },
+    plugins: [],
+};
