5caaba89490ef3ee18b9057eee495aaadd8648db
Enmanuel Bautista <148397318+byenmalmao@users.noreply.github.com>
2025-05-03 19:13:31 -0400

    Update Argenis.py

--- Argenis.py
+++ Argenis.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Configuración de la base de datos
 app.config['MYSQL_HOST'] = 'localhost'     #Recordar cambiar con sus datos 
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Bismuto888@#'
+
 app.config['MYSQL_DB'] = 'fidebank'
 
 app.secret_key = 'mysecretkey'
